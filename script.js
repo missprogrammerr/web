50e3adce0c2363c8084bd67488ba132f06285938
missprogrammerr <babithakur246@gmail.com>
2023-09-08 12:05:48 +0545

    Updated code

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ form.addEventListener("submit", function(e){
         email.nextElementSibling.innerText = "Email is required.";
         e.preventDefault();
     }
+    //email validation with regex
+    let email_pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let validate_email = email_pattern.test();
+    if(validate_email == false){
+        email.nextElementSibling.innerText = "Email is not valid.";
+        e.preventDefault();
+    }
+
     if(address.value == ''){
         address.nextElementSibling.innerText = "Address is required.";
         e.preventDefault();
@@ -26,4 +34,29 @@ form.addEventListener("submit", function(e){
         phone.nextElementSibling.innerText = "Phone is required.";
         e.preventDefault();
     }
+});
+
+/**
+ * basic email validation
+ */
+email.addEventListener("keyup",function(){
+    if(this.value.indexOf('@')== -1) {
+        this.nextElementSibling.innerText ="One'@' symbol is require" ;
+    } else if(this.value.indexOf('@') != this.value.indexOf('@')){
+        this.nextElementSibling.innerText ="Only one'@' symbol is require" ;
+    } else if (this.value.indexOf('.') == -1){
+        this.nextElementSibling.innerText ="At least one '.' symbol is require" ;
+    }else {
+        this.nextElementSibling.innerText = "";
+    }
+});
+
+fullName.addEventListener("keyup", function(){
+      let name_pattern = /([A-Z])\w+\s([A-Z])\w+/g;
+      let validate_name = name_pattern.test(fullName.value);
+      if(validate_name == false){
+        this.nextElementSibling.innerText = "Full Name is not valid.";
+      }else{
+        this.nextElementSibling.innerText = "";
+      }
 });
